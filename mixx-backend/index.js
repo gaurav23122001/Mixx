e08324f1b8ed0bf78391828ef19734a7f51331a8
SinghAtithi <mails4rockstararyan@gmail.com>
2022-12-31 21:26:42 +0530

    done something something

--- mixx-backend/index.js
+++ mixx-backend/index.js
@@ -6,16 +6,17 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const { authRouter } = require('./routes/auth.js');
+const { projectRouter } = require('./routes/project/createProject.js');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 /* Routes */
 
-app.get('/register', authRouter);
-app.get('/login', authRouter);
+app.use('/auth', authRouter);
+app.use('/project', projectRouter);
 
 
 app.get('/extract-audio', (req, res) => {
--- mixx-backend/routes/project/createProject.js
+++ mixx-backend/routes/project/createProject.js
@@ -5,4 +5,7 @@ const projectRouter = express.Router();
 
 projectRouter.post('/create', verify, (req, res) => {
       console.log(req.body);
+      res.status(200).send("Project Created");
 })
+
+module.exports = projectRouter;
