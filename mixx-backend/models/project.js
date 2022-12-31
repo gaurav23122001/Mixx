f9b25ef5b52b1dbd3ca705980b44d5f6ca7b3ff3
SinghAtithi <mails4rockstararyan@gmail.com>
2022-12-31 16:55:59 +0530

    Comment in project model

--- mixx-backend/models/project.js
+++ mixx-backend/models/project.js
@@ -13,10 +13,13 @@ const projectSchema = new mongoose.Schema({
             type: String,
             required: true
       },
+      /* Implemented both timestamp and comment as an array of objects
+       [{timestamp: 0, comment: "This is a comment"}, {timestamp: 1, comment: "This is another comment"}]
+       we could also have made it two separate arrays, but I think this is better */
       timeStampAndComment: {
             type: Array,
             required: false
-      },
+      },    
       user: {
             type: String,
             required: true
