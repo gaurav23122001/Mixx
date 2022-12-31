edb3d9fcc6a3033d6e57140a3676bd25981efac9
SinghAtithi <mails4rockstararyan@gmail.com>
2022-12-31 17:03:07 +0530

    Added Error Codes

---
+++ mixx-backend/errorCodes.js
@@ -0,0 +1,19 @@
+const errorCodes = {
+      400: "Bad Request",
+      401: "Unauthorized",
+      403: "Forbidden",
+      404: "Not Found",
+      405: "Method Not Allowed",
+      406: "Not Acceptable",
+      408: "Request Timeout",
+      409: "Conflict",
+      410: "Gone",
+      411: "Length Required",
+      412: "Precondition Failed",
+      413: "Payload Too Large",
+      414: "URI Too Long",
+      415: "Unsupported Media Type",
+      416: "Range Not Satisfiable",
+      417: "Expectation Failed",
+      418
+}
