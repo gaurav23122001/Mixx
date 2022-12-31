61287a62f8ef71e3cc120af522aba60adce36e03
Maniteja <katammaniteja@gmail.com>
2022-12-31 21:48:34 +0530

    sidebar

--- mixx-frontend/src/pages/Page.tsx
+++ mixx-frontend/src/pages/Page.tsx
@@ -1,30 +1,84 @@
-import { IonButtons, IonContent, IonGrid, IonHeader, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import { useEffect } from 'react';
-import { Redirect, useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import { LoginMetadata } from '../Models/LoginMetadata';
-import Home from './Home';
-import './Page.css';
+import React, { useState } from "react";
+import { AiFillHome, AiFillContacts } from "react-icons/ai";
+import { SiFiles } from "react-icons/si";
+import "./Page.css";
+import {
+  MdFactCheck,
+  MdOutlineKeyboardArrowRight,
+  MdKeyboardArrowLeft,
+} from "react-icons/md";
+import { LoginMetadata } from "../Models/LoginMetadata";
+import { StorageService } from "../Services/StorageService";
+import { useLocation } from "react-router-dom";
+import { GoogleLogout } from "react-google-login";
 
 interface PageProps {
   loginfunction: (loginMetadata: LoginMetadata | null) => void;
-  loginMetadata: LoginMetadata
+  loginMetadata: LoginMetadata;
 }
 
-const Page: React.FC<PageProps> = ({
-  loginfunction,
-  loginMetadata
-}) => {
+const Page: React.FC<PageProps> = ({ loginMetadata, loginfunction }) => {
+  const location = useLocation();
+  const clientId =
+    "413463613463-mgrsltc9uf95ieghf1iqk0k7bgps5ul9.apps.googleusercontent.com";
+  const logOut = () => {
+    StorageService.Logout();
+    loginfunction(new LoginMetadata("-1"));
+  };
 
-  const { name } = useParams<{ name: string; }>();
-  if (name == "home") {
-    return (
-      <Home loginfunction={loginfunction} loginMetadata={loginMetadata} />
-    );
-  }
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const handleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
   return (
-    <Redirect to="/home" />
+    <div className="container1">
+      <div className={sidebarOpen ? "sidebar sidebar-open" : "sidebar"}>
+        <div className="sidebar-top"></div>
+        <div className="sidebar-top-icon" onClick={handleSidebar}>
+          {sidebarOpen ? (
+            <MdKeyboardArrowLeft size="1.5em" />
+          ) : (
+            <MdOutlineKeyboardArrowRight size="1.5em" />
+          )}
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <span className="icon">
+                <AiFillHome size="1.2em" />
+              </span>
+              <span className="nav-title">Home</span>
+            </li>
+            <li>
+              <span className="icon">
+                <SiFiles size="1.2em" />
+              </span>
+              <span className="nav-title">Files</span>
+            </li>
+            <li>
+              <span className="icon">
+                <MdFactCheck size="1.2em" />
+              </span>
+              <span className="nav-title">About</span>
+            </li>
+            <li>
+              <span className="icon">
+                <AiFillContacts size="1.2em" />
+              </span>
+              <span className="nav-title">Contact</span>
+            </li>
+          </ul>
+          <GoogleLogout
+            clientId={clientId}
+            buttonText="Log out"
+            onLogoutSuccess={logOut}
+          />
+        </nav>
+      </div>
+      <div className="main"></div>
+    </div>
   );
 };
 
