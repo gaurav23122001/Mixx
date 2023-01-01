5c8e98fb7434d2a8c83f0aed56e35a1d2c7e4581
Ankit Gautam <a53gau@gmail.com>
2023-01-02 02:03:18 +0530

    fixed merge conflicts

--- mixx-backend/index.js
+++ mixx-backend/index.js
@@ -1,38 +1,5 @@
 const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-const authRouter = require('./routes/auth.js');
-const projectRouter = require('./routes/project/createProject.js');
-
-
-app.use(express.json());
-app.use(cors());
-dotenv.config();
-app.use('/auth', authRouter);
-app.use('/project', projectRouter);
-
-
-const PORT = process.env.PORT || 5005;
-mongoose.set("strictQuery", true);
-mongoose
-    .connect(process.env.CONNECTION_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() =>
-        app.listen(PORT, () => {
-            console.log(`Server running at http://localhost:${PORT}`);
-        })
-    )
-
-
-
-/* Routes */
-=======
 const cors = require('cors');
 require('dotenv').config();
 const morgan = require("morgan");
@@ -42,26 +9,17 @@ const uploadFileToBucket = require('./controllers/storageBucket');
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const mongoose = require('mongoose');
 
+const authRouter = require('./routes/auth.js');
+const projectRouter = require('./routes/project/createProject.js');
 
 app.use(cors({
     origin: 'http://localhost:8100',
 }));
 app.use(express.json());
 
-app.post('/upload-file', upload, async (req, res) => {
-    // specify the output format here
-    const audioFormat = req.body.audioFormat;
-    
-    try {
-        const filePath = await extractAudioFromFile(req.file.filename, audioFormat)
-        const url = await uploadFileToBucket(filePath, audioFormat);
-        res.status(200).json(url)
-    } catch (error) {
-        res.status(500).json(error + " error index file")
-    }
-})
+
 
 app.post('/upload-url', async (req, res) => {
     const videoUrl = req.body.videoUrl;
@@ -77,16 +35,41 @@ app.post('/upload-url', async (req, res) => {
 })
 
 
+
+app.post('/upload-file', upload, async (req, res) => {
+    // specify the output format here
+    const audioFormat = req.body.audioFormat;
+
+    try {
+        const filePath = await extractAudioFromFile(req.file.filename, audioFormat)
+        const url = await uploadFileToBucket(filePath, audioFormat);
+        res.status(200).json(url)
+    } catch (error) {
+        res.status(500).json(error + " error index file")
+    }
+})
+
+app.use('/auth', authRouter);
+app.use('/project', projectRouter);
+
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
 
+const PORT = process.env.PORT || 5000;
 
-io.on('connection', (socket) => {
-    console.log('a user connected' + socket.id);
-});
+mongoose.set("strictQuery", true);
+mongoose
+    .connect(process.env.CONNECTION_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        server.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}`);
+        })
+    )
+    .catch((err) => console.log(err));
 
 
