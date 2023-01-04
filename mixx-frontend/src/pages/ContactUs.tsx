ea9c0a5eebf668476bcbcd39ef45098074bb7b7b
Maniteja <katammaniteja@gmail.com>
2023-01-04 21:55:49 +0530

    committed

--- mixx-frontend/src/pages/ContactUs.tsx
+++ mixx-frontend/src/pages/ContactUs.tsx
@@ -1,22 +1,18 @@
-
-import Menu from '../components/Menu';
-import { LoginMetadata } from '../Models/LoginMetadata';
+import Menu from "../components/Menu";
+import { LoginMetadata } from "../Models/LoginMetadata";
 
 interface ContactProps {
-    loginfunction: (loginMetadata: LoginMetadata | null) => void;
-    loginMetadata: LoginMetadata
+  loginfunction: (loginMetadata: LoginMetadata | null) => void;
+  loginMetadata: LoginMetadata;
 }
 
-const Contact: React.FC<ContactProps> = ({
-    loginfunction,
-    loginMetadata
-}) => {
-    return (
-        <div className="container1">
-            <Menu loginMetadata={loginMetadata} loginfunction={loginfunction} />
-            <div className="main">sdasdasdasdasdasdasdasdsa</div>
-        </div>
-    );
+const Contact: React.FC<ContactProps> = ({ loginfunction, loginMetadata }) => {
+  return (
+    <div className="container1">
+      <Menu loginMetadata={loginMetadata} loginfunction={loginfunction} />
+      <div className="main">xx</div>
+    </div>
+  );
 };
 
 export default Contact;
