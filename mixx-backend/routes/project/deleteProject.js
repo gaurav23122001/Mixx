f399780682373fe844009d0f4ef3d8920748def2
SinghAtithi <mails4rockstararyan@gmail.com>
2023-01-07 14:36:24 +0530

    delete project api

--- mixx-backend/routes/project/deleteProject.js
+++ mixx-backend/routes/project/deleteProject.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const verify = require('../../middleware/verify');
 const Project = require('../../models/project');
+const User = require('../../models/user')
 
 const deleteProjectRouter = express.Router();
 
 
 const deleteProject = async (req, res) => {
-      const { projectId } = req.body;
+      const { projectId, userId } = req.body;
       try {
             const project = await Project
                   .findOne({
                         _id: projectId
                   }).exec();
             if (project) {
+                  const user = await User.findOne({ _id: userId }).exec();
+                  if (user) {
+                        User.updateOne({ _id: userId }, { $pull: { savedProjects: projectId } }).exec();
+                  }
                   await project.deleteOne();
                   res.status(200).send("Project Deleted");
             }
@@ -27,7 +32,7 @@ const deleteProject = async (req, res) => {
 }
 
 
-deleteProjectRouter.post('/delete',  (req, res) => {
+deleteProjectRouter.post('/delete', (req, res) => {
       const { projectId } = req.body;
       // console.log(projectId)
       // console.log(req.body);
